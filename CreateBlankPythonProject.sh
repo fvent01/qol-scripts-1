e367b29980209e96a961119fbf61d506ac1a3002
Francois Venter <francoiseventer@gmail.com>
2026-06-14 15:21:30 -0500

    Update CreateBlankPythonProject.sh

--- CreateBlankPythonProject.sh
+++ CreateBlankPythonProject.sh
@@ -1,55 +1,138 @@
-# Ask the user for the name of the project.
-echo "Enter a name for the Blank Python Project:"
-read ProjectName
+#!/bin/bash
 
-# Check if the directory .venvs exists.
-if [ ! -d "./.venvs" ]; then
-  # If not, then ask the use rif they want to create the directory.
-  while true; do
-    echo ".venvs directory does not exist."
-    echo "Do you want to create it? (yes|no)"
-    read answer
+# ============================================================
+# Blank Python Project Creator
+# Author: Francois Ernst Venter
+# Version: 2.0.0
+# ============================================================
 
-  # If the answer is yes, then create the directory and break the loop.
-    if [ "$answer" = "yes" ]; then
-      mkdir ./.venvs
-      break
-    elif [ "$answer" = "no" ]; then
-      # If the answer is no, then exit the script.
-      echo ".venvs directory is required."
-      clear
-      echo "Exiting..."
-      exit 1
-    else
-      echo "Invalid input."
-      echo "Please answer only yes or no."
-    fi
-done
+set -e
+
+# ------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+pause() {
+    echo
+    read -rp "Press Enter to continue..."
+}
+
+# ------------------------------------------------------------
+# Check Dependencies
+# ------------------------------------------------------------
+
+if ! command_exists python3; then
+    echo "ERROR: python3 is not installed."
+    echo "Please install Python 3 and try again."
+    exit 1
 fi
 
-clear
+# ------------------------------------------------------------
+# Get Project Name
+# ------------------------------------------------------------
 
-# Navigate to the directory where you want to create the virtual environment
-echo "Entering '.venv' Directory..."
-cd ./.venvs/
+echo
+read -rp "Enter a name for the Python Project: " ProjectName
 
-# Create the virtual environment
-clear && echo "Creating the virtual environment '$ProjectName'..."
-python3 -m venv ./$ProjectName
-clear && echo "The virtual environment $ProjectName has been created."
+if [[ -z "$ProjectName" ]]; then
+    echo "Project name cannot be empty."
+    exit 1
+fi
 
-# Activate the virtual environment
-cd $ProjectName
-clear && echo "Activating " && source ./bin/activate
+# ------------------------------------------------------------
+# Check/Create .venvs Directory
+# ------------------------------------------------------------
 
-# Update PIP so not to run into funny business.
-echo "Updating PIP..."
-python3 -m pip install --upgrade pip
-clear && echo "PIP has finished updating..."
+if [[ ! -d "./.venvs" ]]; then
 
-clear && echo "Congrats! Django has successfully installed."
-echo ""
-echo "Program written by:"
-echo "Francois Ernst Venter 8/8/2023"
-echo ""
+    echo
+    echo ".venvs directory does not exist."
 
+    while true; do
+        read -rp "Create it now? (yes/no): " answer
+
+        case "${answer,,}" in
+            yes|y)
+                mkdir -p ./.venvs
+                echo "Created .venvs directory."
+                break
+                ;;
+            no|n)
+                echo
+                echo ".venvs directory is required."
+                echo "Exiting..."
+                exit 1
+                ;;
+            *)
+                echo "Please answer yes or no."
+                ;;
+        esac
+    done
+fi
+
+# ------------------------------------------------------------
+# Create Virtual Environment
+# ------------------------------------------------------------
+
+echo
+echo "Entering .venvs directory..."
+
+cd ./.venvs || exit 1
+
+if [[ -d "$ProjectName" ]]; then
+    echo
+    echo "ERROR: Virtual environment '$ProjectName' already exists."
+    exit 1
+fi
+
+echo
+echo "Creating virtual environment '$ProjectName'..."
+
+python3 -m venv "$ProjectName"
+
+echo "Virtual environment created successfully."
+
+# ------------------------------------------------------------
+# Activate Environment
+# ------------------------------------------------------------
+
+echo
+echo "Activating virtual environment..."
+
+source "./$ProjectName/bin/activate"
+
+# ------------------------------------------------------------
+# Upgrade pip
+# ------------------------------------------------------------
+
+echo
+echo "Updating pip..."
+
+python -m pip install --upgrade pip
+
+echo
+echo "pip updated successfully."
+
+# ------------------------------------------------------------
+# Finished
+# ------------------------------------------------------------
+
+echo
+echo "========================================"
+echo "Project Name : $ProjectName"
+echo "Location     : $(pwd)/$ProjectName"
+echo "Python       : $(python --version)"
+echo "========================================"
+echo
+echo "Virtual environment is active."
+echo
+echo "To activate it later:"
+echo "source ./.venvs/$ProjectName/bin/activate"
+echo
+echo "Happy coding!"
+echo
+echo "Written by Francois Ernst Venter"
